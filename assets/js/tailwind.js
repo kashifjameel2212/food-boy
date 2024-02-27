a841a5932d91be929f3c312b01c16c606b8185f7
kashif <03214752508a@gmail.com>
2024-02-27 18:36:51 +0500

    firet commit

---
+++ assets/js/tailwind.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        },
+        container: {
+          padding: {
+            DEFAULT: '1rem',
+            sm: '2rem',
+            lg: '4rem',
+            xl: '5rem',
+            '2xl': '6rem',
+          },
+          center:true,
+        },
+        backgroundImage: {
+            'homebg': "url('/assets/images/carousel-1.jpg')",
+            'bgmain': "url('/assets/images/html.jpg')",
+          }
+      }
+    }
+  }
+
+
+
+
+
+ 
